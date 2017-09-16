31d79b911799ef51bbd2af5fa1c21394dac19be6
bryutus <bryutus@gmail.com>
2017-09-17 01:43:09 +0900

    APIの返却値にDBのデータを返すように設定
    アルバム情報を返す

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"regexp"
 
+	"github.com/bryutus/caspian-back/app/db"
+	"github.com/bryutus/caspian-back/app/models"
 	"github.com/labstack/echo"
 )
 
@@ -15,7 +17,18 @@ func GetAlbums() echo.HandlerFunc {
 			return c.JSON(http.StatusOK, fmt.Sprintf("Invalid value %s", limit))
 		}
 
-		return c.JSON(http.StatusOK, j)
+		db := db.Connect()
+		defer db.Close()
+
+		// 取得データ確認
+		h := models.History{}
+		db.Where("resource_type = ?", "album").Last(&h)
+
+		// 取得データ確認
+		r := []models.Resource{}
+		db.Where("history_id = ?", h.ID).Limit(limit).Find(&r)
+
+		return c.JSON(http.StatusOK, r)
 	}
 }
 
