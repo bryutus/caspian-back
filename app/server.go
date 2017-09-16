bfb37e41fd6c9242ed8618aa3c1cc07db24d3523
bryutus <bryutus@gmail.com>
2017-09-16 14:45:37 +0900

    CORSを追加
    ・echo/middlewareを利用して機能追加
    ・コンテナに依存パッケージを追加

--- app/server.go
+++ app/server.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/bryutus/caspian-back/app/handler"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"localhost"},
+		AllowMethods: []string{echo.GET},
+	}))
 
 	e.GET("/album", handler.GetAlbum())
 	e.GET("/song", handler.GetSong())
