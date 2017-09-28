5d0f063411541d7da4ef6f53ddb91c815d68c9f1
bryutus <bryutus@gmail.com>
2017-09-28 23:47:38 +0900

    外部APIからの取得処理のテストをデータ駆動の書き方に変更

--- app/batch/fetch_test.go
+++ app/batch/fetch_test.go
@@ -3,7 +3,19 @@ package main
 import "testing"
 
 func TestParseDatetime(t *testing.T) {
-	if got, want := parseDatetime(""), "2006-01-02 15:04:05"; got != want {
-		t.Errorf("parseDatetime(\"\"): got %v want %v", got, want)
+	testCases := []struct {
+		desc string
+		in   string
+		want string
+	}{
+		{"空文字列を渡して2006-01-02 15:04:05が返却されること", "", "2006-01-02 15:04:05"},
+		{"文字列の2017-09-26T04:21:40Zを渡して2017-09-26 04:21:40が返却されること", "2017-09-26T04:21:40Z", "2017-09-26 04:21:40"},
+	}
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			if got := parseDatetime(test.in); got != test.want {
+				t.Errorf("parseDatetime(\"%v\"): got [%v] want [%v]", test.in, got, test.want)
+			}
+		})
 	}
 }
